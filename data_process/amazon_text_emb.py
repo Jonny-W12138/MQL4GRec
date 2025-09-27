867ee0859c339bc5855bf274e8e0d8009ce4e036
jonnyw <jonny_wong_@outlook.com>
2025-09-27 09:58:02 +0800

    parallel_text_emb

--- data_process/amazon_text_emb.py
+++ data_process/amazon_text_emb.py
@@ -1,4 +1,3 @@
-import argparse
 import collections
 import gzip
 import html
@@ -12,17 +11,23 @@ import numpy as np
 from utils import *
 from transformers import LlamaForCausalLM, LlamaTokenizer, LlamaConfig, AutoTokenizer, AutoModel
 
+# 全局变量
+DATASET = 'Sports'
+ROOT = "./"
+GPU_IDS = [0, 1]  # 使用两个GPU
+PLM_NAME = 'llama'
+MODEL_NAME_OR_PATH = 'huggyllama/llama-7b'
+MODEL_CACHE_DIR = '/kaggle/tmp/cache_model'
+MAX_SENT_LEN = 2048
+WORD_DROP_RATIO = -1  # 默认不丢弃单词
 
-def load_data(args):
-
-    item2feature_path = os.path.join(args.root, f'{args.dataset}.item.json')
+def load_data():
+    item2feature_path = os.path.join(ROOT, f'{DATASET}.item.json')
     item2feature = load_json(item2feature_path)
-
     return item2feature
 
 def generate_text(item2feature, features):
     item_text_list = []
-
     for item in item2feature:
         data = item2feature[item]
         text = []
@@ -30,26 +35,20 @@ def generate_text(item2feature, features):
             if meta_key in data:
                 meta_value = clean_text(data[meta_key])
                 text.append(meta_value.strip())
-
         item_text_list.append([int(item), text])
-
     return item_text_list
 
-def preprocess_text(args):
+def preprocess_text():
     print('Process text data: ')
-    print(' Dataset: ', args.dataset)
-
-    item2feature = load_data(args)
-    # load item text and clean
+    print(' Dataset: ', DATASET)
+    item2feature = load_data()
     item_text_list = generate_text(item2feature, ['title', 'description'])
-    # item_text_list = generate_text(item2feature, ['title'])
-    # return: list of (item_ID, cleaned_item_text)
     return item_text_list
 
-def generate_item_embedding(args, item_text_list, tokenizer, model, word_drop_ratio=-1):
+def generate_item_embedding(item_text_list, tokenizer, model, word_drop_ratio=-1):
     print(f'Generate Text Embedding: ')
-    print(' Dataset: ', args.dataset)
-
+    print(' Dataset: ', DATASET)
+    
     items, texts = zip(*item_text_list)
     order_texts = [[0]] * len(items)
     for item, text in zip(items, texts):
@@ -58,19 +57,17 @@ def generate_item_embedding(args, item_text_list, tokenizer, model, word_drop_ra
         assert text != [0]
 
     embeddings = []
-    start, batch_size = 0, 1
+    start, batch_size = 0, 8  # 增加batch_size以利用多GPU
     with torch.no_grad():
         while start < len(order_texts):
-            if (start+1)%100==0:
-                print("==>",start+1)
+            if (start + batch_size) % 100 == 0:
+                print("==>", start + batch_size)
             field_texts = order_texts[start: start + batch_size]
-            # print(field_texts)
-            field_texts = zip(*field_texts)
+            field_texts = list(zip(*field_texts))
     
             field_embeddings = []
             for sentences in field_texts:
                 sentences = list(sentences)
-                # print(sentences)
                 if word_drop_ratio > 0:
                     print(f'Word drop with p={word_drop_ratio}')
                     new_sentences = []
@@ -84,10 +81,10 @@ def generate_item_embedding(args, item_text_list, tokenizer, model, word_drop_ra
                         new_sent = ' '.join(new_sent)
                         new_sentences.append(new_sent)
                     sentences = new_sentences
-                encoded_sentences = tokenizer(sentences, max_length=args.max_sent_len,
-                                              truncation=True, return_tensors='pt',padding="longest").to(args.device)
+                encoded_sentences = tokenizer(sentences, max_length=MAX_SENT_LEN,
+                                            truncation=True, return_tensors='pt', padding="longest").to(DEVICE)
                 outputs = model(input_ids=encoded_sentences.input_ids,
-                                attention_mask=encoded_sentences.attention_mask)
+                              attention_mask=encoded_sentences.attention_mask)
     
                 masked_output = outputs.last_hidden_state * encoded_sentences['attention_mask'].unsqueeze(-1)
                 mean_output = masked_output.sum(dim=1) / encoded_sentences['attention_mask'].sum(dim=-1, keepdim=True)
@@ -101,41 +98,29 @@ def generate_item_embedding(args, item_text_list, tokenizer, model, word_drop_ra
     embeddings = torch.cat(embeddings, dim=0).numpy()
     print('Embeddings shape: ', embeddings.shape)
 
-    file = os.path.join(args.root, args.dataset + '.emb-' + args.plm_name + "-td" + ".npy")
+    file = os.path.join(ROOT, DATASET + '.emb-' + PLM_NAME + "-td" + ".npy")
     np.save(file, embeddings)
 
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default='Instruments', help='Instruments / Arts / Games')
-    parser.add_argument('--root', type=str, default="/userhome/dataset/MQL4GRec")
-    parser.add_argument('--gpu_id', type=int, default=0, help='ID of running GPU')
-    parser.add_argument('--plm_name', type=str, default='llama')
-    parser.add_argument('--model_name_or_path', type=str, default='huggyllama/llama-7b')
-    parser.add_argument('--model_cache_dir', type=str, default='/userhome/cache_models')
-    parser.add_argument('--max_sent_len', type=int, default=2048)
-    parser.add_argument('--word_drop_ratio', type=float, default=-1, help='word drop ratio, do not drop by default')
-    return parser.parse_args()
-
-
 if __name__ == '__main__':
-    args = parse_args()
-
-    args.root = os.path.join(args.root, args.dataset)
-
-    device = set_device(args.gpu_id)
-    args.device = device
-
-    item_text_list = preprocess_text(args)
-
-    kwargs = {"cache_dir": args.model_cache_dir, "local_files_only": os.path.exists(args.model_cache_dir)}
+    ROOT = os.path.join(ROOT, DATASET)
     
-    plm_tokenizer, plm_model = load_plm(args.model_name_or_path, kwargs)
+    # 设置多GPU设备
+    DEVICE = torch.device(f"cuda:{GPU_IDS[0]}" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.device_count() > 1:
+        print(f"Using {len(GPU_IDS)} GPUs!")
+        os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, GPU_IDS))
+    
+    item_text_list = preprocess_text()
+    
+    kwargs = {"cache_dir": MODEL_CACHE_DIR, "local_files_only": os.path.exists(MODEL_CACHE_DIR)}
+    plm_tokenizer, plm_model = load_plm(MODEL_NAME_OR_PATH, kwargs)
+    
     if plm_tokenizer.pad_token_id is None:
         plm_tokenizer.pad_token_id = 0
-    plm_model = plm_model.to(device)
-
-    generate_item_embedding(args, item_text_list,plm_tokenizer,
-                            plm_model, word_drop_ratio=args.word_drop_ratio)
-
-
+    
+    # 使用DataParallel包装模型以支持多GPU
+    plm_model = plm_model.to(DEVICE)
+    if torch.cuda.device_count() > 1:
+        plm_model = torch.nn.DataParallel(plm_model, device_ids=GPU_IDS)
+    
+    generate_item_embedding(item_text_list, plm_tokenizer, plm_model, word_drop_ratio=WORD_DROP_RATIO)
