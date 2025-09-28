445a2f1cd5afe09a6876e47ac85c6ebd6a3236cf
jonnyw <jonny_wong_@outlook.com>
2025-09-28 19:05:57 +0800

    sasrec

--- seqrec/upload.py
+++ seqrec/upload.py
@@ -1,6 +1,5 @@
 from huggingface_hub import HfApi
 
-api = HfApi(token='')
 api.upload_folder(
     folder_path="/Users/jonnyw/Documents/personal/internship/recommender-system/MQL4GRec/log/Instruments",
     repo_id="JonnyW/Instruments_rqvae",
